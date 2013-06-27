7d28a32ccb0b302540d17de9e25eef53a468bf4c
Mikhail Abramov <mikhail.abramov@baitek.org>
2013-06-27 17:08:24 +0400

    Исправлена ошибка выбора свойства объекта в колонке представлений

--- components/BaseApplication.php
+++ components/BaseApplication.php
@@ -20,8 +20,8 @@ abstract class BaseApplication extends CWebApplication {
   
   private $_params = null;
 
-  public $version = '0.93.3-3';
-  public $versionDate = '26.06.2013';
+  public $version = '0.93.3-4';
+  public $versionDate = '27.06.2013';
 
   public function __construct($config = null) {
     parent::__construct($config);
