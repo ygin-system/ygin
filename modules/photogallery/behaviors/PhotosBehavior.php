bf6483329a4141004889a0930aff51fe134e21f7
=rustavely <=russtavely@gmail.com>
2013-08-05 11:25:24 +0400

    Добаил возможность использовать текстовые ИД объектов в поведении галереии

--- modules/photogallery/behaviors/PhotosBehavior.php
+++ modules/photogallery/behaviors/PhotosBehavior.php
@@ -5,8 +5,27 @@ class PhotosBehavior extends CActiveRecordBehavior {
 
   public function attach($owner) {
     parent::attach($owner);
-    $this->getOwner()->getMetaData()->addRelation('photosList', array(CActiveRecord::HAS_MANY, 'PhotogalleryPhoto', 'id_photogallery_instance', 'select' => 'id_photogallery_photo, name', 'with' => 'image', 'on' => 'photosList.id_photogallery_object = '.$this->idObject, 'joinType' => 'JOIN', 'order' => 'photosList.sequence'));
-    $this->getOwner()->getMetaData()->addRelation('countPhoto', array(CActiveRecord::STAT, 'PhotogalleryPhoto', 'id_photogallery_instance', 'condition' => 'id_photogallery_object = '.$this->idObject));
+    if (empty($this->idObject)) {
+      if ($owner instanceOf DaActiveRecord) {
+        $this->idObject = $owner->getIdObject();
+      }
+    }
+    $this->getOwner()->getMetaData()->addRelation(
+      'photosList',
+      array(CActiveRecord::HAS_MANY, 'PhotogalleryPhoto', 'id_photogallery_instance',
+        'select' => 'id_photogallery_photo, name',
+        'with' => 'image',
+        'on' => 'photosList.id_photogallery_object = :PHOTOGALLERY_OBJECT_ID',
+        'joinType' => 'JOIN',
+        'order' => 'photosList.sequence',
+        'params' => array(':PHOTOGALLERY_OBJECT_ID' => $this->idObject),
+    ));
+    $this->getOwner()->getMetaData()->addRelation(
+      'countPhoto',
+      array(CActiveRecord::STAT, 'PhotogalleryPhoto', 'id_photogallery_instance',
+        'condition' => 'countPhoto.id_photogallery_object = :PHOTOGALLERY_OBJECT_ID',
+        'params' => array(':PHOTOGALLERY_OBJECT_ID' => $this->idObject),
+    ));
     $this->getOwner()->attachBehaviors(array(
       'CascadeDelete' => array(
         'class' => 'CascadeDeleteBehavior',
