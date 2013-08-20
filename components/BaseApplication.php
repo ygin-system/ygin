4032a17fad7747c075f0f121aced85cc23e13a9a
Mikhail Abramov <mikhail.abramov@baitek.org>
2013-08-20 15:50:42 +0400

    Пофиксена переменная в классе VisitSite

--- components/BaseApplication.php
+++ components/BaseApplication.php
@@ -20,8 +20,8 @@ abstract class BaseApplication extends CWebApplication {
   
   private $_params = null;
 
-  public $version = '0.93.6';
-  public $versionDate = '15.08.2013';
+  public $version = '0.93.6-1';
+  public $versionDate = '20.08.2013';
 
   public function __construct($config = null) {
     parent::__construct($config);
