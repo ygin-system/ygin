be362c6bd30d92ee98e9738afbcc2ef9405d1cc3
Mikhail Abramov <mikhail.abramov@baitek.org>
2013-09-17 17:52:53 +0400

    Чуть поправлена реализация комментарев. Теперь моделям необязательно иметь подключенное поведение CommentableRelationBehavior

--- modules/comments/widgets/views/eCommentsListWidget.php
+++ modules/comments/widgets/views/eCommentsListWidget.php
@@ -1,5 +1,5 @@
 <div id="<?=$this->id?>" class="itemComments comment-widget mCommentArea">
-<h2 id="cCommentTag" class="mCommentTag"><a data-object="<?=$this->model->getIdObject();?>" data-instance="<?=$this->model->getIdInstance();?>" href="#" onclick="$('#addCommentDialog-<?=$this->id?>,.mCommentList').slideToggle(); return false"><?php echo Yii::t('CommentsModule.msg', 'Comments') . " (" .$this->model->relationCountComment(). ")";?></a></h2>
+<h2 id="cCommentTag" class="mCommentTag"><a data-object="<?=$this->model->getIdObject();?>" data-instance="<?=$this->model->getIdInstance();?>" href="#" onclick="$('#addCommentDialog-<?=$this->id?>,.mCommentList').slideToggle(); return false"><?php echo Yii::t('CommentsModule.msg', 'Comments') . " (" .$count. ")";?></a></h2>
 <?php
 /* $this->isObjectBlog = ($this->model->getIdObject() == BlogPost::ID_OBJECT);
 if ($this->isObjectBlog === true && DaUser::isGuest() === true && $this->model->comment_type == BlogPost::COMMENTS_ALLOWED_FOR_AUTHORIZED) {
