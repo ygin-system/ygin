658f807b2299cd377e3ad398eb8709208cd0ff1e
Mikhail Abramov <mikhail.abramov@baitek.org>
2013-09-20 16:16:45 +0400

    Улучшена автогенерация ид экземпляров для разработчиков системы

--- modules/backend/backend/objectParameter/autoPrimaryKey/autoPrimaryKeyWidget.php
+++ modules/backend/backend/objectParameter/autoPrimaryKey/autoPrimaryKeyWidget.php
@@ -9,31 +9,44 @@ class AutoPrimaryKeyWidget extends TextFieldWidget {
   public $prefixAttr = '';
 
   public function init() {
-    if (!YGIN_DEVELOP) {
-      $this->render = false;
-      if ($this->model->isNewRecord) {
-        $config = str_replace(array(' ', ','), ';', $this->getObjectParameter()->getSqlParameter());
-        $arr = explode(';', $config);
-        foreach($arr AS $param) {
-          if (trim($param) == '') continue;
-          if ($this->sourceAttr == null) {
-            $this->sourceAttr = explode('/', $param);
-            continue;
-          }
-          if ($this->parentSourceAttr === null) {
-            if ($param == '-') {
-              $this->parentSourceAttr = false;
-            } else {
-              $this->parentSourceAttr = $param;
-            }
-            continue;
-          }
-          if ($this->prefixAttr == null) $this->prefixAttr = $param.'-';
+    if ($this->model->isNewRecord) {
+      $config = str_replace(array(' ', ','), ';', $this->getObjectParameter()->getSqlParameter());
+      $arr = explode(';', $config);
+      foreach($arr AS $param) {
+        if (trim($param) == '') continue;
+        if ($this->sourceAttr == null) {
+          $this->sourceAttr = explode('/', $param);
+          continue;
         }
+        if ($this->parentSourceAttr === null) {
+          if ($param == '-') {
+            $this->parentSourceAttr = false;
+          } else {
+            $this->parentSourceAttr = $param;
+          }
+          continue;
+        }
+        if ($this->prefixAttr == null) $this->prefixAttr = $param.'-';
       }
+    }
+
+    if (!YGIN_DEVELOP) { // генерируем имя автоматически
+      $this->render = false;
     }
     parent::init();
   }
+  public function run() {
+    if (YGIN_DEVELOP && $this->model->isNewRecord) {
+      $id = 'ygin-';
+      if ($this->parentSourceAttr != null) {
+        $id = $this->model->{$this->parentSourceAttr}.'-';
+      }
+      $id .= $this->prefixAttr;
+      $this->model->{$this->attributeName} = $id;
+    }
+    parent::run();
+  }
+
   public function onPostForm(PostFormEvent $event) {
     if (!YGIN_DEVELOP && $this->model->isNewRecord) {
       // для не разработчиков системы генерируем имя строковых ключей автоматически
