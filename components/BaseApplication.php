8b16d3346d0dbf4a3b8ac47721cd0b13e56b5e5f
Mikhail Abramov <mikhail.abramov@baitek.org>
2013-11-20 13:50:32 +0400

    Добавлено более расширенное описание изменений в модуле комментариев. Обновлена версия

--- components/BaseApplication.php
+++ components/BaseApplication.php
@@ -20,8 +20,8 @@ abstract class BaseApplication extends CWebApplication {
   
   private $_params = null;
 
-  public $version = '0.93.9';
-  public $versionDate = '21.10.2013';
+  public $version = '0.93.9-1';
+  public $versionDate = '20.11.2013';
 
   public function __construct($config = null) {
     parent::__construct($config);
