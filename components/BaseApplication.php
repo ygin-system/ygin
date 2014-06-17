0f7d614f9a7db714e248f4b768b16f2d551c0a4c
Mikhail Abramov <mikhail.abramov@baitek.org>
2014-06-17 12:49:24 +0400

    Новая версия релиза

--- components/BaseApplication.php
+++ components/BaseApplication.php
@@ -20,8 +20,8 @@ abstract class BaseApplication extends CWebApplication {
   
   private $_params = null;
 
-  public $version = '0.93.10-7';
-  public $versionDate = '17.02.2014';
+  public $version = '0.94';
+  public $versionDate = '17.06.2014';
 
   public function __construct($config = null) {
     parent::__construct($config);
